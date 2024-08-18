270db50aef1a4fad85bfcb0950afc58f0839f8a2
Volodymyr Partytskyi <volodymyr.partytskyi@diymaxeda.com>
2024-08-18 17:46:54 +0300

    Add layouts & setup standard linter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,6 +9,6 @@ export default defineConfig({
     react(),
     tailwind({
       applyBaseStyles: false,
-    })
-  ]
-});
+    }),
+  ],
+})
