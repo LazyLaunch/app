c483cd5536df1521e0472893e43cf62f5b593507
Volodymyr Partytskyi <volodimir.partytskyi@gmail.com>
2024-10-23 19:09:04 +0300

    Add contact tabs

---
+++ src/components/contacts/contact-nav-link.tsx
@@ -0,0 +1,41 @@
+import { Database, Users } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
+
+export type ContactNavLinkId = 'contacts' | 'fields'
+
+const ICONS: Record<ContactNavLinkId, any> = {
+  contacts: Users,
+  fields: Database,
+} as const
+
+export function ContactNavLink({
+  label,
+  id,
+  active,
+  url,
+}: {
+  label: string
+  id: ContactNavLinkId
+  active: ContactNavLinkId
+  url: string
+}) {
+  const Comp = ICONS[id]
+  const isActive = id === active
+  return (
+    <Button
+      asChild
+      variant="link"
+      className={cn('space-x-2 rounded-none border-b-2 border-transparent hover:no-underline', {
+        'border-accent-foreground': isActive,
+        'hover:border-accent-foreground/10': !isActive,
+      })}
+    >
+      <a href={url}>
+        <Comp className="size-4" />
+        <span>{label}</span>
+      </a>
+    </Button>
+  )
+}
